2a05377c33a59803dce642f084d2523cd0b87c74
StartApp-SDK <pavlo.kurliandskyi@start.io>
2022-07-04 17:27:20 +0300

    Updated iOS SDK to 4.7.1. Raised version to 2.3.0

--- Assets/Plugins/API/AdSdk.cs
+++ Assets/Plugins/API/AdSdk.cs
@@ -20,7 +20,7 @@ namespace StartApp
 {
     public abstract class AdSdk
     {
-        public static string WrapperVersion = "3.0.0";
+        public static string WrapperVersion = "2.3.0";
 
         private static AdSdk sInstance;
         public static AdSdk Instance
