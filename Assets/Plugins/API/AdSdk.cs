078c10252597613e681ffda6a292afe49bdd1996
tovchenko <tovchenko@outlook.com>
2022-10-25 22:52:21 +0200

    update android sdk to 4.10.4

--- Assets/Plugins/API/AdSdk.cs
+++ Assets/Plugins/API/AdSdk.cs
@@ -20,7 +20,7 @@ namespace StartApp
 {
     public abstract class AdSdk
     {
-        public static string WrapperVersion = "3.0.1";
+        public static string WrapperVersion = "3.1.0";
 
         private static AdSdk sInstance;
         public static AdSdk Instance
