6d149c686b2ce8394513d2cc4908999803ddcd2f
Pavlo Kurlyandskiy <pavlo.kurliandskyi@startapp.com>
2024-07-23 19:36:14 +0300

    Updated iOS SDK to 4.10.3. Raised version to 3.1.5

--- Assets/Plugins/API/AdSdk.cs
+++ Assets/Plugins/API/AdSdk.cs
@@ -20,7 +20,7 @@ namespace StartApp
 {
     public abstract class AdSdk
     {
-        public static string WrapperVersion = "3.1.4";
+        public static string WrapperVersion = "3.1.5";
 
         private static AdSdk sInstance;
         public static AdSdk Instance
